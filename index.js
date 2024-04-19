7076e18cb186bba291beefb7dbaed2a7041daa12
Lakshay <lakshayagarwal298895@gmail.com>
2024-04-19 23:03:47 +0530

    first

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// Import the express module
 const express = require('express')
 
-// Create an instance of express
 const app = express()
 
-// Define a route handler for the root URL
+// request to server
 app.get('/', (req, res) => {
-  res.send('Hello, World!')
+  //getting response from server
+  res.send('Hello World')
 })
 
 app.get('/about', (req, res) => {
-  res.send('This is a simple Express application.')
+  res.send('About Us')
 })
 
-// Start the server on port 3000
+app.get('/home', (req, res) => {
+  res.send('Home')
+})
+
+// listen on 8000
 app.listen(8000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running ')
 })
